dddec0555e6e068d257caea18ddb4ac6d0f3bca2
火线兔 <silverhawk@foxmail.com>
2022-12-14 17:24:04 +0800

    新增签名算法

--- ticket.go
+++ ticket.go
@@ -1,12 +1,19 @@
 package AossGoSdk
 
+import (
+	"errors"
+	jsoniter "github.com/json-iterator/go"
+	Net "github.com/tobycroft/TuuzNet"
+	"time"
+)
+
 func Wechat_ticket_signature(project, noncestr string, timestamp time.Time, url string) (string, error) {
 	post := map[string]any{
 		"noncestr":  noncestr,
 		"timestamp": timestamp,
 		"url":       url,
 	}
-	ret, err := Net.Post(baseUrl+unlimited_file, map[string]interface{}{
+	ret, err := Net.Post(baseUrl+ticket_signature, map[string]interface{}{
 		"token": project,
 	}, post, nil, nil)
 	if err != nil {
