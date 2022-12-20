1244a93685718c69cd7667a23a7550e79f788c19
火线兔 <silverhawk@foxmail.com>
2022-12-20 15:43:20 +0800

    新增签名算法

--- image.go
+++ image.go
@@ -6,6 +6,21 @@ import (
 	Net "github.com/tobycroft/TuuzNet"
 )
 
+type Canvas_Type_Text struct {
+	Type     string `json:"type"`
+	Text     string `json:"text,omitempty"`
+	Position string `json:"position,omitempty"`
+	X        int    `json:"x"`
+	Y        int    `json:"y"`
+}
+
+type Canvas_Type_Image struct {
+	Type string `json:"type"`
+	URL  string `json:"url,omitempty"`
+	X    int    `json:"x"`
+	Y    int    `json:"y"`
+}
+
 // Canvas_url:获取微信小程序二维码（302方法，推荐占用少）
 func Canvas_url(project, width int64, height int64, background string, data string) (string, error) {
 	post := map[string]any{
