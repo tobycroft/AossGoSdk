276ebc695e7744f417c5a527374c17651943b968
火线兔 <silverhawk@foxmail.com>
2023-03-23 14:32:49 +0800

    新增Lcic功能

--- lcic.go
+++ lcic.go
@@ -1,7 +1,46 @@
 package AossGoSdk
 
-func Lcic_CreateUser(Name, OriginId, Avatar interface{}) {
+import (
+	"encoding/json"
+	"errors"
+	"github.com/tobycroft/Calc"
+	Net "github.com/tobycroft/TuuzNet"
+	"time"
+)
 
+type Lcic struct {
+	Name  string
+	Token string
+}
+
+func (self *Lcic) Lcic_CreateUser(Name, OriginId, Avatar interface{}) error {
+	ts := time.Now().Unix()
+	param := map[string]any{
+		"Name":     Name,
+		"OriginId": OriginId,
+		"Avatar":   Avatar,
+		"ts":       ts,
+		"name":     self.Name,
+		"sign":     Calc.Md5(self.Token + Calc.Any2String(ts)),
+	}
+	ret, err := Net.Post(baseUrls+"/v1/sms/single/push", nil, param, nil, nil)
+	//fmt.Println(ret, err)
+	if err != nil {
+		return err
+	} else {
+		var rs ret_struct
+		errs := json.Unmarshal([]byte(ret), &rs)
+		if errs != nil {
+			return errors.New(ret)
+		} else {
+			//fmt.Println(rs)
+			if rs.Code == 0 {
+				return nil
+			} else {
+				return errors.New(rs.Echo)
+			}
+		}
+	}
 }
 
 func Lcic_RoomCreate(TeacherId, StartTime, EndTime, Name interface{}) {
