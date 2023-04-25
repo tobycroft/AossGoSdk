0c3df6c41143311c3a9317883c518c2db9106d44
火线兔 <silverhawk@foxmail.com>
2023-04-25 12:02:00 +0800

    新增Live直播功能

--- live.go
+++ live.go
@@ -1,7 +1,6 @@
 package AossGoSdk
 
 import (
-	"encoding/json"
 	"errors"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/tobycroft/Calc"
@@ -14,7 +13,7 @@ type Live struct {
 	Code  string
 }
 
-type liveStructCreateRoom struct {
+type liveStructCreateAll struct {
 	Code int                 `json:"code"`
 	Data LiveStructCreateAll `json:"data"`
 	Echo string              `json:"echo"`
@@ -35,146 +34,131 @@ type LiveStructCreateAll struct {
 }
 
 // RoomCreate TeacherId:老师ID | StartTime:开始时间int | EndTime:结束时间int | Name:房间名称
-func (self *Lcic) CreateAll() (LcicStructCreateRoom, error) {
+func (self *Lcic) CreateAll(title any) (LiveStructCreateAll, error) {
+	ts := time.Now().Unix()
+	param := map[string]any{
+		"title": title,
+		"ts":    ts,
+		"name":  self.Name,
+		"sign":  Calc.Md5(self.Token + Calc.Any2String(ts)),
+	}
+	ret, err := Net.Post(baseUrls+"/v1/live/room/create_all", map[string]interface{}{
+		"token": self.Name,
+	}, param, nil, nil)
+	//fmt.Println(ret, err)
+	if err != nil {
+		return LiveStructCreateAll{}, err
+	} else {
+		var resp ret_std
+		err = jsoniter.UnmarshalFromString(ret, &resp)
+		if err != nil {
+			return LiveStructCreateAll{}, errors.New(ret)
+		}
+		if resp.Code == 0 {
+			var dat liveStructCreateAll
+			err = jsoniter.UnmarshalFromString(ret, &dat)
+			if err != nil {
+				return LiveStructCreateAll{}, errors.New(ret)
+			}
+			return dat.Data, nil
+		} else {
+			return LiveStructCreateAll{}, errors.New(resp.Echo)
+		}
+	}
+}
+
+type liveStructCreateRoom struct {
+	Code int                  `json:"code"`
+	Data LiveStructCreateRoom `json:"data"`
+	Echo string               `json:"echo"`
+}
+
+type LiveStructCreateRoom struct {
+	Rtmp        string `json:"rtmp"`
+	Domain      string `json:"domain"`
+	ObsServer   string `json:"obs_server"`
+	StreamCode  string `json:"stream_code"`
+	Webrtc      string `json:"webrtc"`
+	Srt         string `json:"srt"`
+	RtmpOverSrt string `json:"rtmp_over_srt"`
+}
+
+// CreateRoom创建房间并返回房间数据
+func (self *Lcic) CreateRoom(title any) (LiveStructCreateRoom, error) {
+	ts := time.Now().Unix()
+	param := map[string]any{
+		"title": title,
+		"ts":    ts,
+		"name":  self.Name,
+		"sign":  Calc.Md5(self.Token + Calc.Any2String(ts)),
+	}
+	ret, err := Net.Post(baseUrls+"/v1/live/room/create", map[string]interface{}{
+		"token": self.Name,
+	}, param, nil, nil)
+	//fmt.Println(ret, err)
+	if err != nil {
+		return LiveStructCreateRoom{}, err
+	} else {
+		var resp ret_std
+		err = jsoniter.UnmarshalFromString(ret, &resp)
+		if err != nil {
+			return LiveStructCreateRoom{}, errors.New(ret)
+		}
+		if resp.Code == 0 {
+			var dat liveStructCreateRoom
+			err = jsoniter.UnmarshalFromString(ret, &dat)
+			if err != nil {
+				return LiveStructCreateRoom{}, errors.New(ret)
+			}
+			return dat.Data, nil
+		} else {
+			return LiveStructCreateRoom{}, errors.New(resp.Echo)
+		}
+	}
+}
+
+type liveStructPlayUrl struct {
+	Code int               `json:"code"`
+	Data LiveStructPlayUrl `json:"data"`
+	Echo string            `json:"echo"`
+}
+
+type LiveStructPlayUrl struct {
+	PlayDomain string `json:"play_domain"`
+	PlayFlv    string `json:"play_flv"`
+	PlayHls    string `json:"play_hls"`
+	PlayRtmp   string `json:"play_rtmp"`
+}
+
+// CreateRoom创建房间并返回房间数据
+func (self *Lcic) GetPlayUrl() (LiveStructPlayUrl, error) {
 	ts := time.Now().Unix()
 	param := map[string]any{
 		"ts":   ts,
 		"name": self.Name,
 		"sign": Calc.Md5(self.Token + Calc.Any2String(ts)),
 	}
-	ret, err := Net.Post(baseUrls+"/v1/lcic/room/create", map[string]interface{}{
+	ret, err := Net.Post(baseUrls+"/v1/live/room/play_url", map[string]interface{}{
 		"token": self.Name,
 	}, param, nil, nil)
 	//fmt.Println(ret, err)
 	if err != nil {
-		return LcicStructCreateRoom{}, err
+		return LiveStructPlayUrl{}, err
 	} else {
 		var resp ret_std
 		err = jsoniter.UnmarshalFromString(ret, &resp)
 		if err != nil {
-			return LcicStructCreateRoom{}, errors.New(ret)
+			return LiveStructPlayUrl{}, errors.New(ret)
 		}
 		if resp.Code == 0 {
-			var dat liveStructCreateRoom
+			var dat liveStructPlayUrl
 			err = jsoniter.UnmarshalFromString(ret, &dat)
 			if err != nil {
-				return LcicStructCreateRoom{}, errors.New(ret)
+				return LiveStructPlayUrl{}, errors.New(ret)
 			}
 			return dat.Data, nil
 		} else {
-			return LcicStructCreateRoom{}, errors.New(resp.Echo)
-		}
-	}
-}
-
-// Lcic_RoomModify RoomId:房间ID | TeacherId:老师ID | StartTime:开始时间int | EndTime:结束时间int | Name:房间名称
-func (self *Lcic) Lcic_RoomModify(RoomId, TeacherId, StartTime, EndTime, Name interface{}) (bool, error) {
-	ts := time.Now().Unix()
-	param := map[string]any{
-		"RoomId":    RoomId,
-		"Name":      Name,
-		"TeacherId": TeacherId,
-		"StartTime": StartTime,
-		"EndTime":   EndTime,
-		"ts":        ts,
-		"name":      self.Name,
-		"sign":      Calc.Md5(self.Token + Calc.Any2String(ts)),
-	}
-	ret, err := Net.Post(baseUrls+"/v1/lcic/room/create", map[string]interface{}{
-		"token": self.Name,
-	}, param, nil, nil)
-	//fmt.Println(ret, err)
-	if err != nil {
-		return false, err
-	} else {
-		var rs ret_std
-		errs := json.Unmarshal([]byte(ret), &rs)
-		if errs != nil {
-			return false, errs
-		} else {
-			//fmt.Println(rs)
-			if rs.Code == 0 {
-				return true, nil
-			} else {
-				return false, errors.New(rs.Echo)
-			}
-		}
-	}
-}
-
-// Lcic_RoomDelete RoomId:房间ID
-func (self *Lcic) Lcic_RoomDelete(RoomId interface{}) (bool, error) {
-	ts := time.Now().Unix()
-	param := map[string]any{
-		"RoomId": RoomId,
-		"ts":     ts,
-		"name":   self.Name,
-		"sign":   Calc.Md5(self.Token + Calc.Any2String(ts)),
-	}
-	ret, err := Net.Post(baseUrls+"/v1/lcic/room/delete", map[string]interface{}{
-		"token": self.Name,
-	}, param, nil, nil)
-	//fmt.Println(ret, err)
-	if err != nil {
-		return false, err
-	} else {
-		var rs ret_std
-		errs := json.Unmarshal([]byte(ret), &rs)
-		if errs != nil {
-			return false, errs
-		} else {
-			//fmt.Println(rs)
-			if rs.Code == 0 {
-				return true, nil
-			} else {
-				return false, errors.New(rs.Echo)
-			}
-		}
-	}
-}
-
-type lcicStructLinkUrl struct {
-	Code int               `json:"code"`
-	Data LcicStructLinkUrl `json:"data"`
-	Echo string            `json:"echo"`
-}
-
-type LcicStructLinkUrl struct {
-	Web string `json:"web"`
-	Pc  string `json:"pc"`
-}
-
-// Lcic_LinkUrl OriginId:学生id | TeacherId:老师id
-func (self *Lcic) Lcic_LinkUrl(OriginId, TeacherId interface{}) (LcicStructLinkUrl, error) {
-	ts := time.Now().Unix()
-	param := map[string]any{
-		"OriginId":  OriginId,
-		"TeacherId": TeacherId,
-		"ts":        ts,
-		"name":      self.Name,
-		"sign":      Calc.Md5(self.Token + Calc.Any2String(ts)),
-	}
-	ret, err := Net.Post(baseUrls+"/v1/lcic/room/delete", map[string]interface{}{
-		"token": self.Name,
-	}, param, nil, nil)
-	//fmt.Println(ret, err)
-	if err != nil {
-		return LcicStructLinkUrl{}, err
-	} else {
-		var resp ret_std
-		err = jsoniter.UnmarshalFromString(ret, &resp)
-		if err != nil {
-			return LcicStructLinkUrl{}, errors.New(ret)
-		}
-		if resp.Code == 0 {
-			var dat lcicStructLinkUrl
-			err = jsoniter.UnmarshalFromString(ret, &dat)
-			if err != nil {
-				return LcicStructLinkUrl{}, errors.New(ret)
-			}
-			return dat.Data, nil
-		} else {
-			return LcicStructLinkUrl{}, errors.New(resp.Echo)
+			return LiveStructPlayUrl{}, errors.New(resp.Echo)
 		}
 	}
 }
