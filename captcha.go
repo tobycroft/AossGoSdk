807d92c0cb23455846bb140b65d91f2043ed1447
Tuuz <2539@tuuz.cn>
2025-12-05 03:30:58 +0800

    添加Captcha的CheckInTime方法并完成迁移状态配置文件

--- captcha.go
+++ captcha.go
@@ -33,6 +33,28 @@ func (self *Captcha) Check(ident, code any) error {
 	}
 }
 
+// Check if the error returned not-nil that represents the captcha has errors
+func (self *Captcha) CheckInTime(ident, code any, validtime_in_second int) error {
+	param := map[string]any{
+		"ident":  ident,
+		"code":   code,
+		"second": validtime_in_second,
+		"token":  self.Token,
+	}
+	rets := new(Net.Post).PostFormDataAny(baseUrl+"/v1/captcha/auth/check", nil, param, nil, nil)
+	var rs ret_std
+	err := rets.RetJson(&rs)
+	if err != nil {
+		return err
+	}
+	//fmt.Println(rs)
+	if rs.Code == 0 {
+		return nil
+	} else {
+		return errors.New(rs.Echo)
+	}
+}
+
 // CheckWithCode This function will return the httpcode if the captcha is not valid, the code will be returned as -103, code 200 for network error, code 500 for json decode error
 func (self *Captcha) CheckWithCode(ident, code any) (int64, error) {
 	param := map[string]any{
