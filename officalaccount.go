c91c5350b29cba4129aad72912970d82c65638b0
Tuuz <2539@tuuz.cn>
2025-12-05 02:41:58 +0800

    优化网络请求处理，替换jsoniter为bytedance/sonic，简化错误处理逻辑并更新函数签名

--- officalaccount.go
+++ officalaccount.go
@@ -3,6 +3,7 @@ package AossGoSdk
 import (
 	"errors"
 
+	"github.com/bytedance/sonic"
 	Net "github.com/tobycroft/TuuzNet"
 )
 
@@ -62,23 +63,20 @@ type wechatDataMapStringInterface struct {
 }
 
 // Wechat_offi_get_user_list:获取已关注用户的openid列表，slices仅限openid无法区别谁是谁
-func Wechat_offi_get_user_list(project string) ([]string, error) {
+func Wechat_offi_get_user_list(project string) (str []string, err error) {
 	rets := new(Net.Post).PostFormDataAny(baseUrl+offi_user_list, map[string]interface{}{
 		"token": project,
 	}, nil, nil, nil)
-	if err != nil {
-		return nil, err
-	}
 	var resp ret_std
 	err = rets.RetJson(&resp)
 	if err != nil {
-		return nil, errors.New(ret)
+		return
 	}
 	if resp.Code == 0 {
 		var dat wechatDataSlices
-		err = jsoniter.UnmarshalFromString(ret, &dat)
+		err = rets.RetJson(&dat)
 		if err != nil {
-			return nil, errors.New(ret)
+			return
 		}
 		return dat.Data, nil
 	} else {
@@ -100,26 +98,23 @@ type WechatUserInfo struct {
 }
 
 // Wechat_offi_get_user_info:微信获取某个用户的详细情况（但是有可能获取不到）
-func Wechat_offi_get_user_info(project, openid string) (WechatUserInfo, error) {
+func Wechat_offi_get_user_info(project, openid string) (info WechatUserInfo, err error) {
 	post := map[string]any{
 		"openid": openid,
 	}
 	rets := new(Net.Post).PostFormDataAny(baseUrl+offi_user_list, map[string]interface{}{
 		"token": project,
 	}, post, nil, nil)
-	if err != nil {
-		return WechatUserInfo{}, err
-	}
 	var resp ret_std
 	err = rets.RetJson(&resp)
 	if err != nil {
-		return WechatUserInfo{}, errors.New(ret)
+		return
 	}
 	if resp.Code == 0 {
 		var dat wechatDataUserInfo
-		err = jsoniter.UnmarshalFromString(ret, &dat)
+		err = rets.RetJson(&dat)
 		if err != nil {
-			return WechatUserInfo{}, errors.New(ret)
+			return
 		}
 		return dat.Data, nil
 	} else {
@@ -128,7 +123,7 @@ func Wechat_offi_get_user_info(project, openid string) (WechatUserInfo, error) {
 }
 
 // Wechat_offi_openidUrl:redirect_url无需urlencode，AOSS会自动urlencode
-func Wechat_offi_openidUrl(project, redirect_uri, response_type, scope, state string, show_in_qrcode bool) (string, error) {
+func Wechat_offi_openidUrl(project, redirect_uri, response_type, scope, state string, show_in_qrcode bool) (str string, err error) {
 	post := map[string]any{
 		"redirect_uri":  redirect_uri,
 		"response_type": response_type,
@@ -139,17 +134,14 @@ func Wechat_offi_openidUrl(project, redirect_uri, response_type, scope, state st
 	rets := new(Net.Post).PostFormDataAny(baseUrl+offi_openid_url, map[string]interface{}{
 		"token": project,
 	}, post, nil, nil)
-	if err != nil {
-		return "", err
-	}
 	var resp ret_std
 	err = rets.RetJson(&resp)
 	if err != nil {
-		return "", errors.New(ret)
+		return
 	}
 	if resp.Code == 0 {
 		var wwuf wechatStringData
-		err = jsoniter.UnmarshalFromString(ret, &wwuf)
+		err = rets.RetJson(&wwuf)
 		if err != nil {
 			return "", err
 		}
@@ -160,26 +152,23 @@ func Wechat_offi_openidUrl(project, redirect_uri, response_type, scope, state st
 }
 
 // Wechat_offi_openid_from_code:通过微信前置授权取回code后，通过该code获取openid
-func Wechat_offi_openid_from_code(project, code any) (string, error) {
+func Wechat_offi_openid_from_code(project, code any) (str string, err error) {
 	post := map[string]any{
 		"code": code,
 	}
 	rets := new(Net.Post).PostFormDataAny(baseUrl+offi_openid_aquire_from_code, map[string]interface{}{
 		"token": project,
 	}, post, nil, nil)
-	if err != nil {
-		return "", err
-	}
 	var resp ret_std
 	err = rets.RetJson(&resp)
 	if err != nil {
-		return "", errors.New(ret)
+		return
 	}
 	if resp.Code == 0 {
 		var wwuf wechatStringData
-		err = jsoniter.UnmarshalFromString(ret, &wwuf)
+		err = rets.RetJson(&wwuf)
 		if err != nil {
-			return "", err
+			return
 		}
 		return wwuf.Data, nil
 	} else {
@@ -193,8 +182,8 @@ type Wechat_template_data_struct struct {
 }
 
 // Wechat_template_send:微信发送模版功能
-func Wechat_template_send(project, openid, template_id, url interface{}, data map[string]Wechat_template_data_struct) (string, error) {
-	dat, _ := jsoniter.MarshalToString(data)
+func Wechat_template_send(project, openid, template_id, url interface{}, data map[string]Wechat_template_data_struct) (str string, err error) {
+	dat, _ := sonic.MarshalString(data)
 	post := map[string]any{
 		"openid":      openid,
 		"template_id": template_id,
@@ -204,17 +193,14 @@ func Wechat_template_send(project, openid, template_id, url interface{}, data ma
 	rets := new(Net.Post).PostFormDataAny(baseUrl+offi_template_send, map[string]interface{}{
 		"token": project,
 	}, post, nil, nil)
-	if err != nil {
-		return "", err
-	}
 	var resp ret_std
 	err = rets.RetJson(&resp)
 	if err != nil {
-		return "", errors.New(ret)
+		return
 	}
 	if resp.Code == 0 {
 		var wwuf wechatStringData
-		err = jsoniter.UnmarshalFromString(ret, &wwuf)
+		err = rets.RetJson(&wwuf)
 		if err != nil {
 			return "", err
 		}
@@ -225,7 +211,7 @@ func Wechat_template_send(project, openid, template_id, url interface{}, data ma
 }
 
 // Wechat_snsAuth:验证前端获取到的accesstoken和openid是否正确匹配
-func Wechat_snsAuth(project, access_token, openid interface{}) (string, error) {
+func Wechat_snsAuth(project, access_token, openid interface{}) (str string, err error) {
 	post := map[string]any{
 		"openid":       openid,
 		"access_token": access_token,
@@ -233,19 +219,16 @@ func Wechat_snsAuth(project, access_token, openid interface{}) (string, error) {
 	rets := new(Net.Post).PostFormDataAny(baseUrl+sns_auth, map[string]interface{}{
 		"token": project,
 	}, post, nil, nil)
-	if err != nil {
-		return "", err
-	}
 	var resp ret_std
 	err = rets.RetJson(&resp)
 	if err != nil {
-		return "", errors.New(ret)
+		return
 	}
 	if resp.Code == 0 {
 		var wwuf wechatStringData
-		err = jsoniter.UnmarshalFromString(ret, &wwuf)
+		err = rets.RetJson(&wwuf)
 		if err != nil {
-			return "", err
+			return
 		}
 		return wwuf.Data, nil
 	} else {
