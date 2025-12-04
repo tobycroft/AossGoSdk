22016071862748302e3b5d2d9dbca7ea5d4b1ca1
Tuuz <2539@tuuz.cn>
2025-12-05 02:41:34 +0800

    优化base64解码函数签名，明确返回值类型

--- base64.go
+++ base64.go
@@ -4,7 +4,7 @@ import (
 	"encoding/base64"
 )
 
-func decode(data string) ([]byte, error) {
+func decode(data string) (b []byte, err error) {
 	ret, err := base64.StdEncoding.DecodeString(data)
 	if err != nil {
 		return nil, err
