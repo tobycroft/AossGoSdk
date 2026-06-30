83f8039bdbc4106300f038799c6336f1ef226a4e
tobycroft <24893075+tobycroft@users.noreply.github.com>
2026-06-30 13:02:06 +0800

    add GetUploadUrl method to File struct

--- file.go
+++ file.go
@@ -25,6 +25,16 @@ type FileData struct {
 	ExpiredAt string `json:"expired_at"`
 }
 
+type fileUrlRet struct {
+	Code int64       `json:"code"`
+	Data FileUrlData `json:"data"`
+	Echo string      `json:"echo"`
+}
+
+type FileUrlData struct {
+	UploadUrl string `json:"upload_url"`
+}
+
 func (self *File) GetUploadToken() (FileData, error) {
 	ts := time.Now().Unix()
 	sign := Calc.Md5(self.Appid + self.Token + Calc.Any2String(ts))
@@ -55,4 +65,36 @@ func (self *File) GetUploadToken() (FileData, error) {
 		return dat.Data, nil
 	}
 	return FileData{}, errors.New(rs.Echo)
+}
+
+func (self *File) GetUploadUrl() (FileUrlData, error) {
+	ts := time.Now().Unix()
+	sign := Calc.Md5(self.Appid + self.Token + Calc.Any2String(ts))
+
+	param := map[string]any{
+		"appid":     self.Appid,
+		"timestamp": Calc.Any2String(ts),
+		"sign":      sign,
+	}
+
+	remoteUrl := baseUrls
+	if self.RemoteUrl != "" {
+		remoteUrl = self.RemoteUrl
+	}
+
+	rets := new(Net.Post).PostFormDataAny(remoteUrl+"/v2/file/token/upload_url", nil, param, nil, nil)
+	var rs ret_std
+	err := rets.RetJson(&rs)
+	if err != nil {
+		return FileUrlData{}, err
+	}
+	if rs.Code == 0 {
+		var dat fileUrlRet
+		err = rets.RetJson(&dat)
+		if err != nil {
+			return FileUrlData{}, err
+		}
+		return dat.Data, nil
+	}
+	return FileUrlData{}, errors.New(rs.Echo)
 }
